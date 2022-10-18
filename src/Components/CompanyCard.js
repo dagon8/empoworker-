4b6578bbcc9d5b97d621cede76dda2bf31db6be2
dagon8 <danieldanyg@gmail.com>
2022-10-18 12:04:50 -0400

    Added the company card component to folder

---
+++ src/Components/CompanyCard.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function CompanyCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.name}
+        </Typography>
+        <Typography variant="h10" component="div" color="text.secondary">
+          {props.location}
+        </Typography>
+        <Typography variant="h10" component="div" color="text.secondary">
+          {props.industry}
+        </Typography>
+        <br></br>
+        <Typography variant="body2">
+          {props.description}
+        </Typography>
+        <Typography variant="body2">
+          {props.violations}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
