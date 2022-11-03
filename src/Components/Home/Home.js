766b42f0448827eb2333d912084176eb070774b9
dagon8 <danieldanyg@gmail.com>
2022-11-03 13:26:49 -0400

    Modified UI of home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './Home.css'
+
+
+
+export default function Home() {
+  return (
+    <div className='home-container'>
+      <div className='left-home'>       
+        <p className='left-title'>Empoworker</p>
+        <p className='left-sub'>View violations <br/> & prevent working in bad ones.</p>
+      </div>
+
+      <div className='right-home'>
+        <p className='right-title'>Start searching <br/> your company</p>
+      </div>
+    </div>
+  );
+}
