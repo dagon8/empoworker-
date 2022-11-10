a9ada910ddc37afa52979af6f493cee29e69edab
aonnadi <aninja1008@gmail.com>
2022-11-10 04:50:13 -0500

    Add categories to the resources page, need to fix the css of the resources box and the quiz

--- src/Components/Resources/Resources.js
+++ src/Components/Resources/Resources.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import Dropdown from "./Dropdown.js"
 import Quiz from './Quiz.js'
+import './Resources.css'
 
 const Resources = () => {
   return (
-    <div>
-        <div style={{backgroundColor: "#18332D",  height: "100vh",  position: "absolute", left: "20vh", right: "20vh", top: "20vh"}}>
+    <div className='container'>
+        <div className="resources-box" style={{backgroundColor: "#18332D",  height: "100vh",  position: "relative", left: "20vh", right: "20vh", top: "20vh"}}>
+        {/* <div style={{backgroundColor: "#18332D",  height: "100vh",  position: "absolute", left: "20vh", right: "20vh", top: "20vh"}}> */}
         <h1 style={{color: "white",}}>Resources</h1>
         
-        <div style={{display: "grid", gridTemplateColumns: "70vh 30vh", gridAutoRows: "50vh 30vh",  position: "absolute", left: "15vw", right: "15vw", top: "20vh"}}>
+        <div className="categories" style={{display: "grid", gridTemplateColumns: "70vh 30vh", gridAutoRows: "50vh 30vh", positive: "relative", left: "15vw", right: "15vw", top: "20vh"}}>
+          {/* for the category number  */}
+        <Dropdown num={0}/>
         <Dropdown num={1}/>
         <Dropdown num={2}/>
         <Dropdown num={3}/>
         <Dropdown num={4}/>
         </div>
         
-        
         </div>
 
-        <div>
+        <div className='quiz-form'>
           <Quiz></Quiz>
         </div>
     </div>
