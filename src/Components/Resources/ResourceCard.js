f73b48f76e2ffdd9610b7ca1347ca6803b9617b9
aonnadi <aninja1008@gmail.com>
2022-11-21 14:59:20 -0500

    Resolve issues caused from resources-quiz branch merge

--- src/Components/Resources/ResourceCard.js
+++ src/Components/Resources/ResourceCard.js
@@ -7,7 +7,7 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Box from "@mui/material/Box";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import "./ResourceCard.css";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { t } from 'i18next'
 import { useTranslation } from "react-i18next";
 
@@ -18,66 +18,47 @@ export default function ResourceCard({ category }) {
   let title = "resources_" + transid + "_title"
   let description = "resources_" + transid + "_des"
   return (
-    <div>
-      <Card
-        sx={{
-          backgroundColor: "#98a2b3",
-          display: "flex",
-          width: "1000px",
-          height: "136px",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            backgroundColor: "#d3ced0",
-            width: "220px",
-            height: "136px",
-            position: "absolute",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <InventoryIcon />
-        </Box>
-
-export default function ResourceCard({category}){
-    const navigate = useNavigate();
-    return(
-        
         <div>
            <Card 
-           sx={{ backgroundColor: "#bcd2e4", display:"flex", width:"1000px", height:"136px"}}>
+           sx={{ backgroundColor: "#bcd2e4",
+                 display:"flex", 
+                 width:"1000px", 
+                 height:"136px"}}>
             
-                <Box sx={{display: "flex", flexDirection:"row", flexWrap: "wrap", backgroundColor: "#d3ced0",
-                        width: "220px", height:"136px", position:"absolute", alignItems:"center", justifyContent:"center"}}>
+                <Box sx={{display: "flex", 
+                        flexDirection:"row", 
+                        flexWrap: "wrap", 
+                        backgroundColor: "#d3ced0",
+                        width: "220px",
+                         height:"136px", 
+                         position:"absolute", 
+                         alignItems:"center", 
+                         justifyContent:"center"}}>
                     <InventoryIcon />
                 </Box>
 
                <div className='card-content'>
-          <CardContent>
-            <Typography
-              sx={{
-                fontSize: "22px",
-                fontWeight: "bold",
-                marginLeft: "205px",
-                padding: "16px",
-              }}
-            >
-              {t(title)}
-            </Typography>
-            <Typography
-              sx={{
-                typography: "body2",
-                color: "#343741",
-                marginLeft: "220px",
-              }}
-            >
-              {t(description)}
-            </Typography>
-          </CardContent>
+                    <CardContent>
+                        <Typography
+                        sx={{
+                            fontSize: "22px",
+                            fontWeight: "bold",
+                            marginLeft: "205px",
+                            padding: "16px",
+                        }}
+                        >
+                        {t(title)}
+                        </Typography>
+                        <Typography
+                        sx={{
+                            typography: "body2",
+                            color: "#343741",
+                            marginLeft: "220px",
+                        }}
+                        >
+                        {t(description)}
+                        </Typography>
+                    </CardContent>
                 </div>
                 
                 <CardActions sx={{display:"flex", justifyContent:"flex-end"}}>
@@ -87,14 +68,6 @@ export default function ResourceCard({category}){
                     sx={{color: "#ff7a40"}} > <ArrowForwardIosIcon/></IconButton>
                 </CardActions>
            </Card>
-
-        <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
-          <IconButton sx={{ color: "#7f39fb" }}>
-            {" "}
-            <ArrowForwardIosIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
     </div>
   );
 }
