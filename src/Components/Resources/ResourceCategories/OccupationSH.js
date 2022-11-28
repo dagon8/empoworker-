71055a991f5127aef612f8954c671ec7f4371e2f
aonnadi <aninja1008@gmail.com>
2022-11-28 10:04:19 -0500

    Fix ui of the resouces categroies pages

--- src/Components/Resources/ResourceCategories/OccupationSH.js
+++ src/Components/Resources/ResourceCategories/OccupationSH.js
@@ -5,10 +5,10 @@ import "../ResourceCategories/ResourcesCSS/Osh.css";
 const OccupationSH = () => {
   return (
     <div className="container">
-        <div className="box">
-          <div className= "header">Resources</div>
-          <div className="osh-title">Occupational Safety & Health</div>
-          <div className="osh-description">Wage theft occurs any time an employer does not pay an employee everything the employee is owed by law. </div>
+        <div className="osh-box">
+          <div className= "header">Occupational Safety & Health</div>
+          {/* <div className="osh-title">Occupational Safety & Health</div> */}
+          <div className="osh-description">Deals with all aspects of health and safety in the workplace. </div>
         </div>
     </div>
    
