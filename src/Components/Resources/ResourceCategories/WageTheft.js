e415a606f8480d2b833992783cc8c9483ab3bae2
aonnadi <aninja1008@gmail.com>
2022-11-28 13:23:22 -0500

    Add some resources links for the wage theft page

--- src/Components/Resources/ResourceCategories/WageTheft.js
+++ src/Components/Resources/ResourceCategories/WageTheft.js
@@ -7,7 +7,29 @@ const WageTheft = () => {
         <div className="wage-box">
           <div className= "header">Wage Theft</div>
           {/* <div className="wage-title">Wage Theft Violation</div> */}
-          <div className="wage-description">Wage theft occurs any time an employer does not pay an employee everything the employee is owed by law. </div>
+          <div className="wage-description">
+            Wage theft occurs any time an employer does not pay an employee everything the employee is owed by law.
+          </div>
+
+          <div className="wage-links">
+            <div className="wage-report">
+                <a href="https://www.mass.gov/how-to/file-a-workplace-complaint" target="_blank" rel="noopener noreferrer">
+                    Report Wage Theft
+                  </a>
+            </div>
+
+            <div className="wage-laws">
+                <a href="https://www.mass.gov/guides/pay-and-recordkeeping" target="_blank" rel="noopener noreferrer">
+                    Massachusetts Wage Laws
+                  </a>
+                  <br></br>
+                  <a href="https://www.mass.gov/guides/breaks-and-time-off" target="_blank" rel="noopener noreferrer">
+                    Breaks and Time Off
+                  </a>
+            </div>
+          </div>
+
+          
         </div>
     </div>
    
