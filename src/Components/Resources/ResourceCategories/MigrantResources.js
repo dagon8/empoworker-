bc55050102953ee2071708d2683e1c9cb00b97fc
aonnadi <aninja1008@gmail.com>
2022-12-11 17:59:42 -0500

    Add links for immgrant resources

--- src/Components/Resources/ResourceCategories/MigrantResources.js
+++ src/Components/Resources/ResourceCategories/MigrantResources.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ResourceCategory.css";
 import Descriptions from "../ViolationGlossary/violationsDescription.json";
-import { Grid, Box } from '@mui/material'
+import { Grid, Box, Typography } from '@mui/material'
 
 
 const MigrantResources = () => {
@@ -14,47 +14,35 @@ const MigrantResources = () => {
       </div>
 
       <div className="category-content">
-        <div className="category-subheader"> What is Wage Theft?</div>
-        <div className="category-des1"> { Descriptions.wage_theft.category_description} </div>
+        <div className="category-subheader"> What are Immigrant Resources?</div>
+        <div className="category-des1"> 
+        <Typography> Once undocumented immigrants' rights are violated, there is legal pathway for undocumented workers. </Typography>
+        </div>
       </div>
 
-      <div className="category-subheader">  Violations under Wage Theft:</div>
+      <div className="category-subheader">  Definitions:</div>
       <div className="line-div"> </div>
       
       <div className="violations-box-w">
       <Box sx={{ width: '100%'}}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.sca_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.sca_violtn_cnt.violation_description}</p>
+          <h4>U Visa</h4>
+          <p>{Descriptions.workplace_accident.violations.osha_violtn_cnt.violation_description}</p>
           <h1></h1>
         </Grid>
         <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.fmla_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.fmla_violtn_cnt.violation_description}</p>
+          <h4>Pro Se</h4>
+          <p>{Descriptions.workplace_accident.violations.crew_violtn_cnt.violation_description}</p>
         </Grid>
         <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.dbra_cl_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.dbra_cl_violtn_cnt.violation_description}</p>
+          <h4>Labor Trafficking</h4>
+          <p>{Descriptions.workplace_accident.violations.osha_violtn_cnt.violation_description}</p>
           <h1></h1>
         </Grid>
         <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.flsa_smw14_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.flsa_smw14_violtn_cnt.violation_description}</p>
-        </Grid>
-        <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.cwhssa_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.cwhssa_violtn_cnt.violation_description}</p>
-          <h1></h1>
-        </Grid>
-        <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.pca_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.pca_violtn_cnt.violation_description}</p>
-        </Grid>
-        <Grid item xs={6}>
-          <h4>{Descriptions.wage_theft.violations.ca_violtn_cnt.expanded_name}</h4>
-          <p>{Descriptions.wage_theft.violations.ca_violtn_cnt.violation_description}</p>
-          <h1></h1>
+          <h4>Something</h4>
+          <p>{Descriptions.workplace_accident.violations.crew_violtn_cnt.violation_description}</p>
         </Grid>
       </Grid>
     </Box>
@@ -64,26 +52,49 @@ const MigrantResources = () => {
     <div className="category-links">
       <ul>
         <li> 
-          <a href="https://www.mass.gov/how-to/file-a-workplace-complaint" target="_blank" rel="noopener noreferrer">
-                    Report Wage Theft 
+          <a href="https://jatwork.org/" target="_blank" rel="noopener noreferrer">
+                    Justice at Work
           </a>
         </li>
 
         <li> 
-          <a href="https://www.mass.gov/guides/pay-and-recordkeeping" target="_blank" rel="noopener noreferrer">
-                  Fair Labor Division Multilingual Resources
+          <a href="http://www.mwc-casa.org/" target="_blank" rel="noopener noreferrer">
+                    Canasta Solidaria (Solidarity Basket)
           </a>
         </li>
         
         <li> 
-          <a href="https://www.mass.gov/guides/pay-and-recordkeeping" target="_blank" rel="noopener noreferrer">
-                  Massachusetts Wage Laws
+          <a href="http://verdeamarelo.org/bwg/" target="_blank" rel="noopener noreferrer">
+                    Brazilian Women's Group
           </a>
         </li>
         
         <li> 
-          <a href="https://www.mass.gov/guides/breaks-and-time-off" target="_blank" rel="noopener noreferrer">
-                  Breaks and Time Off
+          <a href="https://www.ilrc.org/sites/default/files/resources/proseuvisamanual_english.pdf" target="_blank" rel="noopener noreferrer">
+                  Getting a U-Visa (Immigrantion help for victims of crime) - English
+          </a>
+        </li>
+        <li> 
+          <a href="https://www.ilrc.org/sites/default/files/resources/proseuvisamanual_spanish.pdf" target="_blank" rel="noopener noreferrer">
+                  Getting a U-Visa (Immigrantion help for victims of crime) - Spanish
+          </a>
+        </li>
+
+        <li> 
+          <a href="https://cgrs.uchastings.edu/sites/default/files/CGRS%20DV%20Pro%20Se%20Manual_Final_2013.pdf" target="_blank" rel="noopener noreferrer">
+                  Pro Se Manual: Asylum, Withholding of Removal, and Convention Against Torture Protection for Survivors of Domestic Violence - English
+          </a>
+        </li>
+
+        <li> 
+          <a href="https://www.masslegalservices.org/system/files/library/A%20Guide%20for%20Certifiers%20-%20MGL%20258F%20Certification%20for%20Victims%20of%20Violent%20Crime%20and%20Human%20Trafficking.pdf" target="_blank" rel="noopener noreferrer">
+                  Certification for Victims of Violent Crime and Human Trafficking - English
+          </a>
+        </li>
+
+        <li> 
+          <a href="https://www.dhs.gov/immigration-options-victims-crimes" target="_blank" rel="noopener noreferrer">
+                  Immigration Options for Victims of Crimes
           </a>
         </li>
       </ul>
