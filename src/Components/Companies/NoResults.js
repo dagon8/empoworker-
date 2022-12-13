c58e9f19e1a10b4a982a18b5a29a131e2455d662
Roberto Rodas-Herndon <rrh2023@bu.edu>
2022-12-12 22:30:41 -0500

    small translation update

--- src/Components/Companies/NoResults.js
+++ src/Components/Companies/NoResults.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import { t } from "i18next";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const NoResults = () => {
   const { t, i18n } = useTranslation();
@@ -10,12 +11,10 @@ const NoResults = () => {
       {/* <h2>{t("no_result")}</h2> */}
       {/* {t("no_result_mess")} */}
       <h1 style={{ padding: "25px" }}>{t("no_result")}</h1>
-      <p className='no-result-paragraph'>
-        {t("no_result_mess")}
-      </p>
-      <div className='no-result-button'>
-        <h3 className='no-result-h4'>Contact Us</h3>
-      </div>
+      <br />
+      <p className='no-result-paragraph'>{t("no_result_mess")}</p>
+
+      <p className='no-result-paragraph'>{t("no_result_mess2")}</p>
     </Box>
   );
 };
